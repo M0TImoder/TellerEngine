60c6db8ecf72d9aaffbf76a0836033f13e17ffe1
M0TImoder <167790126+M0TImoder@users.noreply.github.com>
2026-09-04 19:41:15 +0900

    Cmake関連を追加

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,17 @@
+# ソースキャッシュ
+if(NOT DEFINED CPM_SOURCE_CACHE AND NOT DEFINED ENV{CPM_SOURCE_CACHE})
+    if(WIN32)
+        set(CPM_SOURCE_CACHE "$ENV{LOCALAPPDATA}/CPM" CACHE PATH "")
+    else()
+        set(CPM_SOURCE_CACHE "$ENV{HOME}/.cache/CPM" CACHE PATH "")
+    endif()
+endif()
+
+include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
+
+CPMUsePackageLock(${PROJECT_SOURCE_DIR}/CPM.lock)
+
+# doctest
+if(TELLER_BUILD_TESTS)
+    CPMGetPackage(doctest)
+endif()
